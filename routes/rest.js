d24aeb87fb3bd98ca31da8b7e3d0939178142f30
theGreatKang <srouc159@gmail.com>
2023-01-13 20:08:59 +0900

    first commit

---
+++ routes/rest.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+require("dotenv").config();
+
+router.post('/matches', async function (req, res, next) {
+    const accessId = req.body.accessId;
+    const matchIds = (await fetch(`https://api.nexon.co.kr/fifaonline4/v1.0/users/${accessId}/matches?matchtype=50&limit=10`,
+        {headers: {'Authorization': process.env.API_KEY}}
+    ).then(i => i.json()))
+    res.send(matchIds);
+});
+
+router.post('/match-infos', async function (req, res, next) {
+    const matchIds = req.body.matchIds;
+    const matchInfos = await Promise.all(matchIds.map(async matchId => {
+        const res = await fetch(`https://api.nexon.co.kr/fifaonline4/v1.0/matches/${matchId}`,
+            {headers: {'Authorization': process.env.API_KEY}})
+        return res.json();
+    }));
+    res.send(matchInfos);
+});
+
+module.exports = router;
