67dda6a4b75aac62c370992cea2c0c59672b99f6
theGreatKang <srouc159@gmail.com>
2023-01-20 01:00:18 +0900

    title 달아주기

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '파피온라인 1대1 전적검색' });
 });
 
 module.exports = router;
